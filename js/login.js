9e71f615a0c0b9193786dc25062e08bee9ddd4c2
Agustin Prado <aguprado@gmail.com>
2014-02-22 18:20:08 -0200

    many changes

---
+++ js/login.js
@@ -0,0 +1,19 @@
+    
+
+/*
+$(document).ready(function() {
+  $.ajax({
+      url: "http://rest-service.guides.spring.io/greeting"
+  }).then(function(data) {
+     $('#greeting-id').val(data.id);
+     $('#greeting-content').val("data.content");
+  });
+});*/
+
+$(document).ready(function() {
+
+	$("#signInButtonId").click(function(){
+		window.location.href = "../views/main.html";
+	});
+
+});
